c888e808683cfefa572b8ab428dc1b29636262de
naren.narapureddy@gmail.com <Bavishavedavihana@13>
2026-03-24 11:02:10 +0530

    deleted files

--- middleware/cors.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-  exit;
-}
-?>
