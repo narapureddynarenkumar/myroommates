204b0fb64413533a8093cd7176332eeb5f2c3d02
narapureddynarenkumar <95970481+narapureddynarenkumar@users.noreply.github.com>
2026-04-05 14:59:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ require('dotenv').config();
 
 const authRoutes = require("./routes/authRoutes");
 const roomRoutes = require('./routes/roomRoutes');
+const memberRoutes = require("./routes/memberRoutes")
+const expenseRoutes = require("./routes/expenseRoutes")
+const roomCategoryRoutes = require("./routes/roomCategoryRoutes")
+const calculationRoutes = require("./routes/calculationRoutes")
 
 
 const port = 3001;
@@ -17,5 +21,10 @@ app.use(express.json());
 
 app.use("/api/user", authRoutes);
 app.use("/api/room", roomRoutes);
+app.use("/api/members", memberRoutes);
+app.use("/api/expenses", expenseRoutes);
+app.use("/api/room-categories", roomCategoryRoutes);
+app.use("/api/split", calculationRoutes);
+
 
 http.listen(port, () => console.log("L", "server running..."));
