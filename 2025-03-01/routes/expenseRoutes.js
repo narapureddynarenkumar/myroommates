434e39bf9358769d0eaf4ab85df10b82a422b6fc
narapureddynarenkumar <95970481+narapureddynarenkumar@users.noreply.github.com>
2026-04-06 09:03:52 +0530

    Delete 2025-03-01 directory

--- 2025-03-01/routes/expenseRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const expenseController = require("../controllers/expenseController")
-
-const controller = new expenseController(db);
--- 2025-03-01/routes/roomRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const room = require('../controllers/roomController');
-
-router.get('/rooms/:user_id', room.getRooms);
-router.post('/rooms', room.createRoom);
-router.put('/rooms', room.updateRoom);
-
-module.exports = router;
